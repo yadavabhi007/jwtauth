73b686d6e059575a5f6a3c7bab049ba063282b5f
Abhishek Yadav <yadav31abhi@gmail.com>
2023-02-25 14:50:33 +0530

    code updated

---
+++ myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+# Create your views here.
+
+class StudentView(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
